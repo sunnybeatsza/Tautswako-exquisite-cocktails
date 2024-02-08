eee682ade068a552ebdd78f0a6eb5846897add16
Tumisho Makgeru <makgerutumisho55@gmail.com>
2024-02-08 14:59:03 +0200

    Basic Navbar

--- tau-frontend/src/App.js
+++ tau-frontend/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Contact } from "./Pages/Contact";
+import { Quote } from "./Pages/Quote";
+import { Services } from "./Pages/Services";
+import { AboutUs } from "./Pages/AboutUs";
 
 function App() {
-  return <div className="App">Tautswako Exquisite cocktails</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Contact" element={<Contact />} />
+        <Route exact path="/Quote" element={<Quote />} />
+        <Route exact path="/Services" element={<Services />} />
+        <Route exact path="/About-us" element={<AboutUs />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ tau-frontend/src/Pages/Quote.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Quote = () => {
+  return (
+    <div>
+      <h1>Quote</h1>
+    </div>
+  );
+};
