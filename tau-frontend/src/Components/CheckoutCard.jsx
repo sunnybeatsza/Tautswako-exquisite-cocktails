1dde7d4bbd388adc8767024066830fcaf205ed1f
Tumisho Makgeru <makgerutumisho55@gmail.com>
2024-03-15 20:57:56 +0200

    Checkout page model

---
+++ tau-frontend/src/Components/CheckoutCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CheckoutCard.css";
+
+export const CheckoutCard = () => {
+  return (
+    <div className="checkoutcard-container">
+      <p>Image/Icon</p>
+      <p>Description</p>
+      <p>Price</p>
+      <p>Remove</p>
+    </div>
+  );
+};
--- tau-frontend/src/Pages/Checkout.jsx
+++ tau-frontend/src/Pages/Checkout.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom/dist";
+import "./Checkout.css";
+import { Navbar } from "../Components/Navbar";
+import { CheckoutCard } from "../Components/CheckoutCard";
 
 export const Checkout = () => {
   const location = useLocation();
@@ -7,15 +10,30 @@ export const Checkout = () => {
 
   return (
     <div>
-      <h1>Checkout Page</h1>
-      {/* Display and use formValues in your checkout page */}
-      <h3>{formValues.Guests}</h3>
-      <h3>{formValues.DrinkPref}</h3>
-      <h3>{formValues.Venue}</h3>
-      <h3>{formValues.Address}</h3>
-      <h3>{formValues.DrinkOptions}</h3>
-      <h3>{formValues.Duration}</h3>
-      <h3>{formValues.DateTime}</h3>
+      <Navbar />
+      <div className="checkout-outer-container">
+        <div className="checkout-top-container">
+          <p>
+            Your bag <br />
+            TOTAL (4 items): R150.00 <br />
+            Quotes expire after 10 days.
+          </p>
+        </div>
+        <div className="checkout-bottom-container">
+          <div className="quote-summary">
+            <h1>Quote Summary</h1>
+            <CheckoutCard />
+            <CheckoutCard />
+            <CheckoutCard />
+            <CheckoutCard />
+            <CheckoutCard />
+            <CheckoutCard />
+          </div>
+          <div className="quote-total">
+            <h1>Quote total</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
